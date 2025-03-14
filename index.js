8ab40f144f7c81fca19e6b18ed83c90387a36f09
NJUGUNA NJOROGE <engbrian01@gmail.com>
2025-03-14 23:45:03 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+  function superbowlWin(Array){
+    for(const record of Array){
+    if (record.result === "W"){
+    return record.year;
+    }
+  }
+  }
+  console.log(record.find(superbowlWin));
+    
+    
